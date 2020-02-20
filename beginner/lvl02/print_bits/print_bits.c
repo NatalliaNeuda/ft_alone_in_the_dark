2fee32ee3eed2601072eff645c9d247d9d0eb36e
nneuda <natallia.neuda@gmail.com>
2020-02-20 15:56:08 -0800

    mistakes fixed

--- beginner/lvl02/print_bits/print_bits.c
+++ beginner/lvl02/print_bits/print_bits.c
@@ -4,18 +4,18 @@ void	print_bits(unsigned char octet)
 {
     unsigned char div;
 
-    div = 0b0000001;
-    while (div !=0)
+    div = 0b10000000;
+    while (div != 0)
     {
         if (div & octet)
             write(1, "1", 1);
         else
             write (1, "0", 1);
-        div <<= 1;
+        div >>= 1;
     }
 }
 
-int main(void)
-{
-    print_bits(129);
-}
+// int main(void)
+// {
+//     print_bits(128);
+// }
--- beginner/lvl04/itoa/itoa.c
+++ beginner/lvl04/itoa/itoa.c
@@ -36,12 +36,8 @@ char	*ft_itoa(int nbr)
         div = (-1) * div;
     }
     else
-    {
-        //nbr = nbr * (-1);
         s[i] = '0';     
-    }
     i = 0;
-    // div = nbr;
 	while (div > 1)
 	{ 
 		s[len - (1 + i)] = '0'  + ((div % 10));
@@ -62,3 +58,58 @@ int main(void)
 
    return (0);	
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int numlen(int nb)
+// {
+// 	int len = 1;
+
+// 	while (nb < -9)
+// 	{
+// 		nb /= 10;
+// 		len++;
+// 	}
+// 	return(len);
+
+// }
+
+
+// char	*ft_itoa(int nbr)
+// {
+// 	int sign = 0;
+// 	int len;
+// 	char *s;
+// 	int i = 0;
+// 	int div;
+
+// 	if(nbr < 0)
+// 		sign = 1;
+// 	else
+// 		nbr *= -1;
+// 	div = nbr;
+// 	len = numlen(nbr) + sign;
+// 	s = (char*)malloc(sizeof(char) * len + 1);
+// 	if (sign)
+// 	{
+// 		s[0] = '-';
+// 		// sign = 0;
+// 	}
+// 	while (i < len - sign)
+// 	{
+// 		s[len - i - 1] = -(div % 10) + '0';
+// 		div /= 10;
+// 		i++;
+// 	}
+// 	s[len] = '\0';
+// 	return(s);
+// }
+
+// int main(void)
+// {
+// 	int nbr = 147483648;
+
+// 	printf("%s\n", ft_itoa(nbr));
+// 	return(0);
+// }
